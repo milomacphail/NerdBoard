d14e0f7060b3a2586331e6b9432497c53c60ea29
milomacphail <milomacphail@gmail.com>
2019-09-13 19:02:52 -0400

    debugging

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
-const setAuthToken = token =>{
-    if(token) {
-        //Apply to every request
-        axios.defaults.headers.common['Authorization'] = token;
-    } else {
-        //Delete Auth header
-        delete axios.defaults.headers.commons['Authorization'];
-    }
+const setAuthToken = token => {
+	if (token) {
+		// Apply to every request
+		axios.defaults.headers.common['Authorization'] = token;
+	}
+	else {
+		// Delete Auth Header
+		delete axios.defaults.headers.common['Authorization'];
+	}
 }
 
 export default setAuthToken;
