dc9d4caf8960c9d4a9af0c13270907d017d49e05
milomacphail <milomacphail@gmail.com>
2019-09-22 18:23:33 -0400

    profile fix

--- validation/profile.js
+++ validation/profile.js
@@ -7,9 +7,9 @@ module.exports = function validateProfileInput(data){
     let errors = {};
     
    
-    data.handle = !isEmpty(data.handle) ? data.handle :"";
-    data.status = !isEmpty(data.status) ? data.status: "";
-    data.skills = !isEmpty(data.skills) ? data.skills: "";
+    data.handle = !isEmpty(data.handle) ? data.handle :" ";
+    data.status = !isEmpty(data.status) ? data.status: " ";
+    data.skills = !isEmpty(data.skills) ? data.skills: " ";
     
    
     if (!Validator.isLength(data.handle, {min: 2, max: 40})){
